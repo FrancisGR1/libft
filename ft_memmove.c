f54ca7cbd9b73599efd08cdcf52477f37e87c1aa
Francisco <umnomemuitocriativo@gmail.com>
2023-10-04 10:17:03 +0100

    2nd from home

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,20 +1,36 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: frmiguel <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/03 14:48:05 by frmiguel          #+#    #+#             */
-/*   Updated: 2023/10/03 17:35:06 by frmiguel         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-#include "libft.a"
+#include "libft.h"
 #include <string.h>
+#include <ctype.h>
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	return ;
+	int	len;
+	int	i;
+	unsigned char	*arr;
+	unsigned char	*ptr;
+	
+	i = 0;
+	arr[n];
+	while(*(src + i) && i < n)
+		arr[i++] = *src;
+	i = 0;
+	ptr = dest;
+	while(i < n)
+		*(ptr + i) = arr[i++];	
+	return (dest);
+}
+
+int main (void)
+{
+	char dest1[10];
+	char dest2[10];
+	char arr[] = "Testestesfd";
+	char arr1[] = "TESTETSETEESDF";
+	//ft_memmove(dest1, arr, 5);
+	memmove(dest2, arr, 5);
+	
+	printf("FT: %s\n", dest1);
+	printf("STD: %s\n", dest2);
 }
 
 /*https://www.youtube.com/watch?v=DGwdAQauEV4&ab_channel=PortfolioCourses*/
