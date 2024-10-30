1d693a5f64278f304d96b5576cead2e4403b4863
Francis GR <umnomemuitocriativo@gmail.com>
2024-10-30 08:49:40 +0000

    tweak: more robust ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -24,7 +24,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		s2++;
 		n--;
 	}
-	return (*s1 - *s2);
+	if (n == 0)
+	{
+		return (0);
+	}
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
 /*
 int main (int c, char **v)
