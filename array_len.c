b31e716bdd139100e84582ba4e38f24c883cc982
Francis GR <umnomemuitocriativo@gmail.com>
2024-10-31 13:11:15 +0000

    fix: array_len()

--- array_len.c
+++ array_len.c
@@ -6,7 +6,7 @@
 /*   By: frmiguel <frmiguel@student.42Lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 21:18:51 by frmiguel          #+#    #+#             */
-/*   Updated: 2024/11/10 21:18:51 by frmiguel         ###   ########.fr       */
+/*   Updated: 2024/11/13 21:47:11 by frmiguel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ size_t	array_len(void **arr)
 {
 	size_t	len;
 
+	if (!arr)
+		return (0);
 	len = 0;
-	while (arr[len])
+	while (*(arr + (data_size * len)))
 		len++;
 	return (len);
 }
