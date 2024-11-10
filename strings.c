00da523040c7269b310f618018d12af1199583dc
Francis GR <umnomemuitocriativo@gmail.com>
2024-11-10 21:17:36 +0000

    feat(strings): deep copy

--- strings.c
+++ strings.c
@@ -322,6 +322,18 @@ t_string *string_split_dup(t_string str, char *delimiters, int *len)
 	return (strs);
 }
 
+size_t strs_count(t_string *args)
+{
+	size_t i;
+
+	if (!args)
+		return (0);
+	i = 0;
+	while (args[i].s)
+		i++;
+	return(i);
+}
+
 int str_cmp(t_string s1, t_string s2, size_t s1_start)
 {
 	size_t i;
@@ -452,6 +464,42 @@ void string_arr_free(t_string **strs)
 	}
 	free(*strs);
 	*strs = NULL;
+}
+
+t_string *str_arr_dup(t_string *str_arr)
+{
+	size_t cnt;
+	t_string *str_arr_dup;
+
+	if (!str_arr)
+		return (NULL);
+	cnt = 0;
+	while (str_arr[cnt].s)
+		cnt++;
+	str_arr_dup = ft_calloc(cnt + 1, sizeof(t_string));
+	cnt = 0;
+	while (str_arr[cnt].s)
+	{
+		str_arr_dup[cnt] = str_dup(str_arr[cnt]);
+		cnt++;
+	}
+	str_arr_dup[cnt] = new_str(NULL, 0);
+	return (str_arr_dup);
+
+}
+
+void str_arr_deep_copy(t_string *dst, t_string *src, int size)
+{
+	int i;
+
+	if (!dst || !src || size < 1)
+		return ;
+	i = 0;
+	while (i < size)
+	{
+		dst[i] = str_dup(src[i]);
+		i++;
+	}
 }
 
 //Example usage: string_split
